69a8045fff5e181535e04ac10fd262738a08bb17
NikhilBhandigare <114331715+NikhilBhandigare@users.noreply.github.com>
2023-06-30 16:15:17 +0530

    intial commit

---
+++ homebooking/Frontend/page.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+            $(".more-btn").on('click', function(){
+                $(this).parent().parent().find(".more-text").toggleClass("active");
+            });
+        });
